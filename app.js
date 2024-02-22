f0764668c7c5ac49c39e51b5e7dd36ac39d4aea6
Kiefer Thom <97416146+vagabondtypewriter@users.noreply.github.com>
2024-02-21 17:16:04 -0800

    Completed v1.0

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// packages
 const express = require('express');
 const mysql = require('mysql');
 
@@ -11,10 +10,8 @@ const connection = mysql.createConnection({
 
 const app = express();
 
-// Middleware to parse URL-encoded bodies (for query strings)
 app.use(express.urlencoded({ extended: true }));
 
-// Connect to MySQL
 connection.connect(err => {
   if (err) {
     console.error('Error connecting to MySQL database:', err);
@@ -23,26 +20,21 @@ connection.connect(err => {
   console.log('Connected to MySQL database');
 });
 
-// Your route handler
 app.get('/user', (req, res) => {
+  // TODO add specific functionality for admin panel as requested
   console.log("Got a new request");
 
-  // Extract query parameters
   const { offset, limit } = req.query;
-  // SQL query to select user data with offset and limit
-
 
   console.log("Limit: ", limit);
   console.log("Offset: ", limit);
   let sql = 'SELECT user_id, display_name, hashed_password, email FROM user';
   
-   // Add OFFSET and LIMIT clauses if offset and limit are provided
   if (limit !== undefined) {
     console.log("Sending with limit");
     sql += ` LIMIT ${parseInt(limit)}`;
   }
 
-  // Add OFFSET and LIMIT clauses if offset and limit are provided
   if (offset !== undefined) {
     console.log("Sending with offset");
     sql += ` OFFSET ${parseInt(offset)}`;
@@ -57,13 +49,10 @@ app.get('/user', (req, res) => {
       return;
     }
 
-    // Send the results back to the browser
     res.json(results);
   });
 });
 
-
-// Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
