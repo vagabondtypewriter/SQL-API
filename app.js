c97ec1989a34b32f5288c2939b7d8e11984cb2d3
Kiefer Thom <97416146+vagabondtypewriter@users.noreply.github.com>
2024-03-24 18:39:51 -0700

    check for results val

--- app.js
+++ app.js
@@ -147,7 +147,7 @@ app.get('/getNumUsers', (req, res) => {
       res.status(500).json({ error: 'Internal Server Error' });
       return;
     } else {
-      console.log(res.json(results));
+      console.log(results);
       res.json(results);
     }
   });
