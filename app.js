22aa69f6374f6fa3315c3f9a9b98234f08d2f0d6
Kiefer Thom <97416146+vagabondtypewriter@users.noreply.github.com>
2024-03-24 18:18:10 -0700

    Added both urlencoded and json parsing

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const pool = mysql.createPool({
 const app = express();
 app.use(cors());
 app.use(express.json({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/getUsers', (req, res) => {
   const offset = req.query.offset;
